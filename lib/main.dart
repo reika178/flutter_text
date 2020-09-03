eb1837dcc7ad1084a6a7bc8b1d4a88f0d8778bff
reika178 <rechiko178@gmail.com>
2020-09-04 08:04:56 +0900

    Animation
    P.112まで

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/semantics.dart';
+import 'package:flutter/animation.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,16 +17,61 @@ class LogoApp extends StatefulWidget {
   _LogoAppState createState() => _LogoAppState();
 }
 
-class _LogoAppState extends State<LogoApp> {
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+
+  AnimationController controller;
+  Animation<double> animation;
+
   @override
-  Widget build (BuildContext context) => Scaffold(
+  void initState() {
+    super.initState();
+    controller = AnimationController(duration: Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build (BuildContext context) => GrowTransition(
+    animation: animation,
+    child: LogoWidget(),
+  );
+}
+
+class GrowTransition extends StatelessWidget {
+  GrowTransition({Key key, this.child, this.animation})
+    : assert(child != null),
+      assert(animation != null),
+      super(key: key);
+
+  final Widget child;
+  final Animation<double> animation;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
     body: Center(
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 10),
-        height: 300,
-        width: 300,
-        child: FlutterLogo(),
+      child: AnimatedBuilder(
+        animation: animation,
+        child: child,
+        builder: (context, child) => Container(
+          height: animation.value,
+          width: animation.value,
+          child: child,
+        ),
       ),
     ),
+  );
+}
+
+class LogoWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Container(
+    margin: EdgeInsets.symmetric(vertical: 10),
+    child: FlutterLogo(),
   );
 }
