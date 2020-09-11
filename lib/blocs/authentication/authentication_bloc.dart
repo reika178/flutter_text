2f37792c16fb7fe8f2925781155cbcf5c8831895
reika178 <rechiko178@gmail.com>
2020-09-12 07:46:44 +0900

    チャプター６
    authentication_bloc.dart

--- lib/blocs/authentication/authentication_bloc.dart
+++ lib/blocs/authentication/authentication_bloc.dart
@@ -1,5 +1,53 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
-import 'package:sample_app/blocs/authentication/authentication_event.dart';
-import 'package:sample_app/blocs/authentication/authentication_repository.dart';
-import 'package:sample_app/blocs/authentication/authentication_state.dart';
+import 'package:flutter_text/blocs/authentication/authentication_event.dart';
+import 'package:flutter_text/blocs/authentication/authentication_repository.dart';
+import 'package:flutter_text/blocs/authentication/authentication_state.dart';
+
+class AuthenticationBloc
+    extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final AuthenticationRepository _authRepository;
+
+  AuthenticationBloc({@required AuthenticationRepository authRepository})
+      : assert(authRepository != null),
+        _authRepository = authRepository;
+
+  @override
+  AuthenticationState get initialState => AuthenticationInProgress();
+
+  @override
+  Stream<AuthenticationState> mapEventToState(
+    AuthenticationEvent event,
+  ) async* {
+    if (event is AppStarted) {
+      yield* _mapAppStartedToState();
+    } else if (event is LoggedIn) {
+      yield* _mapLoggedInToState();
+    } else if (event is LoggedOut) {
+      yield* _mapLoggedOutToState();
+    }
+  }
+
+  Stream<AuthenticationState> _mapAppStartedToState() async* {
+    try {
+      final isSignedIn = await _authRepository.isSugnedIn();
+      if (isSignedIn) {
+        final currentUser = await _authRepository.getCurrentUser();
+        yield AuthenticationSuccess(currentUser);
+      } else {
+        yield AithenticationFailure();
+      }
+    } catch (_) {
+      yield AuthenticationFailure();
+    }
+  }
+
+  Stream<AuthenticationState> _mapLoggedInToState() async* {
+    yield AuthenticationSuccess(await _authRepository.getCurrentUser());
+  }
+
+  Stream<AuthenticationState> _mapLoggedOutToState() async* {
+    yield AuthenticationFailure();
+    _authRepository.signOut();
+  }
+    }
