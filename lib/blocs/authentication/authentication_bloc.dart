67180d8e5f03cad82a1a4e0fb2edae56674c058f
reika178 <rechiko178@gmail.com>
2020-09-12 07:32:36 +0900

    チャプター６
    blocs event_list

---
+++ lib/blocs/authentication/authentication_bloc.dart
@@ -0,0 +1,5 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:sample_app/blocs/authentication/authentication_event.dart';
+import 'package:sample_app/blocs/authentication/authentication_repository.dart';
+import 'package:sample_app/blocs/authentication/authentication_state.dart';
